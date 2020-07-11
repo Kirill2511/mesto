8220098972b467718b111afb1b111a3450041997
Kirill <kirill.kirill2@yandex.ru>
2020-07-11 20:20:23 +0500

    fix: all

--- js/script.js
+++ js/script.js
@@ -10,11 +10,16 @@ let profileName = content.querySelector('.profile__title');
 let profileAbout = content.querySelector('.profile__subtitle');
 
 function popupDisplay() {
-    formElement.classList.toggle('popup_close')
+    if (formElement.classList.contains('popup_close')) {
+        nameInput.value = profileName.textContent;
+        aboutInput.value = profileAbout.textContent;
+    }
+    formElement.classList.toggle('popup_close');
 }
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
+
 
     profileName.textContent = nameInput.value;
     profileAbout.textContent = aboutInput.value;
@@ -23,10 +28,6 @@ function formSubmitHandler(evt) {
 }
 
 editButton.addEventListener('click', popupDisplay);
-
-nameInput.value = profileName.textContent;
-aboutInput.value = profileAbout.textContent;
+closeIcon.addEventListener('click', popupDisplay);
 
 formElement.addEventListener('submit', formSubmitHandler);
-
-closeIcon.addEventListener('click', popupDisplay);
