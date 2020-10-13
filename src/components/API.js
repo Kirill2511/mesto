79609c76c996caadf9b9774665e9a5e6be00d7e2
Kirill <kirill.kirill2@yandex.ru>
2020-10-13 23:20:04 +0500

    1

--- src/components/API.js
+++ src/components/API.js
@@ -1,102 +1,102 @@
 export default class API {
-  constructor ({ baseUrl, authorization }) {
-    this._baseUrl = baseUrl
-    this._authorization = authorization
-  }
+	constructor({ baseUrl, authorization }) {
+		this._baseUrl = baseUrl;
+		this._authorization = authorization;
+	}
 
-  _fetch (url, params) {
-    return fetch(this._baseUrl + url, params)
-      .then(res => {
-        if (res.ok) {
-          return res.json()
-        }
-        return Promise.reject(`Ошибка: ${res.status}`)
-      })
-  }
+	_fetch(url, params) {
+		return fetch(this._baseUrl + url, params)
+			.then(res => {
+				if (res.ok) {
+					return res.json();
+				}
+				return Promise.reject(`Ошибка: ${res.status}`);
+			});
+	}
 
-  getInitialUserInfo () {
-    return this._fetch('/users/me', {
-      method: 'GET',
-      headers: {
-        authorization: this._authorization
-      }
-    })
-  }
+	getInitialUserInfo() {
+		return this._fetch('/users/me', {
+			method: 'GET',
+			headers: {
+				authorization: this._authorization,
+			},
+		});
+	}
 
-  editUserInfo (formValues) {
-    return this._fetch('/users/me', {
-      method: 'PATCH',
-      headers: {
-        authorization: this._authorization,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        name: formValues.name,
-        about: formValues.about
-      })
-    })
-  }
+	editUserInfo(formValues) {
+		return this._fetch('/users/me', {
+			method: 'PATCH',
+			headers: {
+				authorization: this._authorization,
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				name: formValues.name,
+				about: formValues.about,
+			}),
+		});
+	}
 
-  editUserAvatar (formValues) {
-    return this._fetch('/users/me/avatar', {
-      method: 'PATCH',
-      headers: {
-        authorization: this._authorization,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        avatar: formValues.avatar
-      })
-    })
-  }
+	editUserAvatar(formValues) {
+		return this._fetch('/users/me/avatar', {
+			method: 'PATCH',
+			headers: {
+				authorization: this._authorization,
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				avatar: formValues.avatar,
+			}),
+		});
+	}
 
-  getInitialCards () {
-    return this._fetch('/cards', {
-      method: 'GET',
-      headers: {
-        authorization: this._authorization
-      }
-    })
-  }
+	getInitialCards() {
+		return this._fetch('/cards', {
+			method: 'GET',
+			headers: {
+				authorization: this._authorization,
+			},
+		});
+	}
 
-  postUserCard (formValues) {
-    return this._fetch('/cards', {
-      method: 'POST',
-      headers: {
-        authorization: this._authorization,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        name: formValues.place,
-        link: formValues.link
-      })
-    })
-  }
+	postUserCard(formValues) {
+		return this._fetch('/cards', {
+			method: 'POST',
+			headers: {
+				authorization: this._authorization,
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				name: formValues.place,
+				link: formValues.link,
+			}),
+		});
+	}
 
-  deleteCard (cardId) {
-    return this._fetch('/cards/' + cardId, {
-      method: 'DELETE',
-      headers: {
-        authorization: this._authorization
-      }
-    })
-  }
+	deleteCard(cardId) {
+		return this._fetch('/cards/' + cardId, {
+			method: 'DELETE',
+			headers: {
+				authorization: this._authorization,
+			},
+		});
+	}
 
-  putLike (cardId) {
-    return this._fetch('/cards/likes/' + cardId, {
-      method: 'PUT',
-      headers: {
-        authorization: this._authorization
-      }
-    })
-  }
+	putLike(cardId) {
+		return this._fetch('/cards/likes/' + cardId, {
+			method: 'PUT',
+			headers: {
+				authorization: this._authorization,
+			},
+		});
+	}
 
-  deleteLike (cardId) {
-    return this._fetch('/cards/likes/' + cardId, {
-      method: 'DELETE',
-      headers: {
-        authorization: this._authorization
-      }
-    })
-  }
+	deleteLike(cardId) {
+		return this._fetch('/cards/likes/' + cardId, {
+			method: 'DELETE',
+			headers: {
+				authorization: this._authorization,
+			},
+		});
+	}
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,188 +1,189 @@
-import './index.css'
-import API from '../components/API.js'
-import Card from '../components/Card.js'
-import FormValidator from '../components/FormValidator.js'
-import Section from '../components/Section.js'
-import PopupWithForm from '../components/PopupWithForm.js'
-import PopupWithImage from '../components/PopupWithImage.js'
-import PopupWithConfirm from '../components/PopupWithConfirm.js'
-import UserInfo from '../components/UserInfo.js'
+// import './index.css';
+import API from '../components/API.js';
+import Card from '../components/Card.js';
+import FormValidator from '../components/FormValidator.js';
+import Section from '../components/Section.js';
+import PopupWithForm from '../components/PopupWithForm.js';
+import PopupWithImage from '../components/PopupWithImage.js';
+import PopupWithConfirm from '../components/PopupWithConfirm.js';
+import UserInfo from '../components/UserInfo.js';
 
 import {
-  authorization,
-  baseUrl,
-  cardContainer,
-  jobInput,
-  nameInput,
-  popupEditAvatar,
-  popupButtonAddCard,
-  popupButtonAvatar,
-  popupButtonConfirm,
-  popupButtonEdit,
-  popupAddCard,
-  popupDeleteCard,
-  popupFormAvatar,
-  popupFormCardNew,
-  popupFormEditProfile,
-  popupZoomCard,
-  popupInputAvatarLink,
-  popupInputNewCard,
-  popupInputNewCardLink,
-  popupParameter,
-  popupEdit,
-  profileAvatar,
-  profileButtonAdd,
-  profileButtonAvatar,
-  profileButtonEdit,
-  profileJob,
-  profileName
-} from '../utils/constants.js'
+	authorization,
+	baseUrl,
+	cardContainer,
+	jobInput,
+	nameInput,
+	popupEditAvatar,
+	popupButtonAddCard,
+	popupButtonAvatar,
+	popupButtonConfirm,
+	popupButtonEdit,
+	popupAddCard,
+	popupDeleteCard,
+	popupFormAvatar,
+	popupFormCardNew,
+	popupFormEditProfile,
+	popupZoomCard,
+	popupInputAvatarLink,
+	popupInputNewCard,
+	popupInputNewCardLink,
+	popupParameter,
+	popupEdit,
+	profileAvatar,
+	profileButtonAdd,
+	profileButtonAvatar,
+	profileButtonEdit,
+	profileJob,
+	profileName,
+} from '../utils/constants.js';
 
-const api = new API({ baseUrl, authorization })
+const api = new API({ baseUrl, authorization });
 
 // Аватар
 const handleAvatarFormSubmit = (formValues) => {
-  return api.editUserAvatar(formValues).then((user) => profile.setUserAvatar(user))
-}
+	return api.editUserAvatar(formValues).then((user) => profile.setUserAvatar(user));
+};
 
-const avatarPopup = new PopupWithForm(popupEditAvatar, handleAvatarFormSubmit)
+const avatarPopup = new PopupWithForm(popupEditAvatar, handleAvatarFormSubmit);
 
 const renderAvatarPopup = () => {
-  popupButtonAvatar.disabled = true
-  avatarValidator.clearError()
-  popupInputAvatarLink.value = ''
-  popupButtonAvatar.classList.add('popup__button_disabled')
-  avatarPopup.open()
-}
+	popupButtonAvatar.disabled = true;
+	avatarValidator.clearError();
+	popupInputAvatarLink.value = '';
+	popupButtonAvatar.classList.add('popup__button_disabled');
+	avatarPopup.open();
+};
 
 // Профиль
-const profile = new UserInfo(profileName, profileJob, profileAvatar)
+const profile = new UserInfo(profileName, profileJob, profileAvatar);
 
-let initialUserId
+let initialUserId;
 
-function setInitialUserId (user) {
-  initialUserId = user._id
+function setInitialUserId(user) {
+	initialUserId = user._id;
 }
 
-function setInitialUser ({ avatar, name, about }) {
-  profile.setUserInfo({ name, about })
-  profile.setUserAvatar({ avatar })
+function setInitialUser({ avatar, name, about }) {
+	profile.setUserInfo({ name, about });
+	profile.setUserAvatar({ avatar });
 }
 
 api
-  .getInitialUserInfo()
-  .then((user) => {
-    setInitialUserId(user)
-    setInitialUser(user)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+	.getInitialUserInfo()
+	.then((user) => {
+		setInitialUserId(user);
+		setInitialUser(user);
+	})
+	.catch((err) => {
+		console.log(err);
+	});
 
 const handleProfileFormSubmit = (formValues) => {
-  return api.editUserInfo(formValues).then((user) => profile.setUserInfo(user))
-}
+	return api.editUserInfo(formValues).then((user) => profile.setUserInfo(user));
+};
 
-const profilePopup = new PopupWithForm(popupEdit, handleProfileFormSubmit)
+const profilePopup = new PopupWithForm(popupEdit, handleProfileFormSubmit);
 
 const renderProfilePopup = () => {
-  const { username, description } = profile.getUserInfo()
-  nameInput.value = username
-  jobInput.value = description
+	const { username, description } = profile.getUserInfo();
+	nameInput.value = username;
+	jobInput.value = description;
 
-  profileValidator.clearError()
-  popupButtonEdit.classList.remove('popup__button_disabled')
+	profileValidator.clearError();
+	popupButtonEdit.classList.remove('popup__button_disabled');
 
-  const event = new Event('input')
-  nameInput.dispatchEvent(event)
-  jobInput.dispatchEvent(event)
+	const event = new Event('input');
+	nameInput.dispatchEvent(event);
+	jobInput.dispatchEvent(event);
 
-  profilePopup.open()
-}
+	profilePopup.open();
+};
 
 // Инициализация картинок
 const card = (cardItem) =>
-  new Card(cardItem, '#template-card', api, {
-    cardUserId: cardItem.owner._id,
-    initialUserId: initialUserId,
-    renderConfirmPopup: renderConfirmPopup,
-    renderImgPopup: renderImgPopup
-  })
+	new Card(cardItem, '#template-card', api, {
+		cardUserId: cardItem.owner._id,
+		initialUserId: initialUserId,
+		renderConfirmPopup: renderConfirmPopup,
+		renderImgPopup: renderImgPopup,
+	});
 
 const renderInitialCards = (cardList) => {
-  const initialCardList = new Section(
-    {
-      items: cardList,
-      renderer: (item) => {
-        const cardElement = card(item).generateCard()
-        initialCardList.addItem(cardElement)
-      }
-    },
-    cardContainer
-  )
+	const initialCardList = new Section(
+		{
+			items: cardList,
+			renderer: (item) => {
+				const cardElement = card(item).generateCard();
+				initialCardList.addItem(cardElement);
+			},
+		},
+		cardContainer,
+	);
 
-  return initialCardList
-}
+	return initialCardList;
+};
 
 api
-  .getInitialCards()
-  .then((cardList) => Promise.all(cardList))
-  .then(renderInitialCards)
-  .then((initialCardList) => initialCardList.renderItems())
-  .catch((err) => {
-    console.log(err)
-  })
+	.getInitialCards()
+	.then((cardList) => Promise.all(cardList))
+	.then(renderInitialCards)
+	.then((initialCardList) => initialCardList.renderItems())
+	.catch((err) => {
+		console.log(err)
+	})
+
 
 // Создать карточку
 const addUserCard = (card, container) => {
-  container.prepend(card)
-}
+	container.prepend(card);
+};
 
 const renderUserCard = (item) => {
-  const cardElement = card(item).generateCard()
-  addUserCard(cardElement, cardContainer)
-}
+	const cardElement = card(item).generateCard();
+	addUserCard(cardElement, cardContainer);
+};
 
 const handleCardFormSubmit = (formValues) => {
-  return api.postUserCard(formValues).then(renderUserCard)
-}
+	return api.postUserCard(formValues).then(renderUserCard);
+};
 
-const cardPopup = new PopupWithForm(popupAddCard, handleCardFormSubmit)
+const cardPopup = new PopupWithForm(popupAddCard, handleCardFormSubmit);
 
 const renderCardPopup = () => {
-  popupButtonAddCard.disabled = true
-  popupButtonAddCard.classList.add('popup__button_disabled')
-  popupInputNewCard.value = ''
-  popupInputNewCardLink.value = ''
-  cardValidator.clearError()
-  cardPopup.open()
-}
+	popupButtonAddCard.disabled = true;
+	popupButtonAddCard.classList.add('popup__button_disabled');
+	popupInputNewCard.value = '';
+	popupInputNewCardLink.value = '';
+	cardValidator.clearError();
+	cardPopup.open();
+};
 
 // Попат с зумом картинки
-const imgPopup = new PopupWithImage(popupZoomCard)
+const imgPopup = new PopupWithImage(popupZoomCard);
 
 const renderImgPopup = ({ link, name }) => {
-  imgPopup.open({ link, name })
-}
+	imgPopup.open({ link, name });
+};
 
 // Удаление карточки
-const confirmPopup = new PopupWithConfirm(popupDeleteCard, popupButtonConfirm)
+const confirmPopup = new PopupWithConfirm(popupDeleteCard, popupButtonConfirm);
 
 const renderConfirmPopup = (callback) => {
-  confirmPopup.open(callback)
-}
+	confirmPopup.open(callback);
+};
 
 // Валидация форм
-const profileValidator = new FormValidator(popupParameter, popupFormEditProfile)
-const cardValidator = new FormValidator(popupParameter, popupFormCardNew)
-const avatarValidator = new FormValidator(popupParameter, popupFormAvatar)
+const profileValidator = new FormValidator(popupParameter, popupFormEditProfile);
+const cardValidator = new FormValidator(popupParameter, popupFormCardNew);
+const avatarValidator = new FormValidator(popupParameter, popupFormAvatar);
 
 // Слушатели
-profileButtonAdd.addEventListener('click', renderCardPopup)
-profileButtonEdit.addEventListener('click', renderProfilePopup)
-profileButtonAvatar.addEventListener('click', renderAvatarPopup)
+profileButtonAdd.addEventListener('click', renderCardPopup);
+profileButtonEdit.addEventListener('click', renderProfilePopup);
+profileButtonAvatar.addEventListener('click', renderAvatarPopup);
 
 // Вызов функций
-profileValidator.enableValidation()
-cardValidator.enableValidation()
-avatarValidator.enableValidation()
+profileValidator.enableValidation();
+cardValidator.enableValidation();
+avatarValidator.enableValidation();
